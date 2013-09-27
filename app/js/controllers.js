cc54bb7036e311b4597d56de063deb72d823829f
J L <jeff@happyboredom.com>
2013-09-27 00:29:17 -0700

    reasonable structure

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,19 +2,29 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', ['$scope', 'Songs', function( $scope, Songs ) {
+angular.module('myApp.controllers', [])
+  // Controller 1
+  .controller('MyCtrl1', ['$scope', 'Songs', function( $scope, Songs ) {
     $scope.songs = Songs; 
   }])
-  .controller('MyCtrl2', ['$scope', '$routeParams', 'Songs',
-    function( $scope, $routeParams, Songs) {
+
+  // Controller 2
+  .controller('MyCtrl2', ['$scope', '$routeParams', '$http', 'Songs',
+    function( $scope, $routeParams, $http, Songs ) {
       $scope.routeParams = $routeParams;
       $scope.songLoad = 'unknown';
-      
+
+      // get lyrics
+      $http.get( '/data/' + $routeParams.slug + '.html')
+        .success( function (response) {
+          $scope.lyrics = response;
+        });
+
       angular.forEach(Songs, function (val, key) {
         if ($routeParams.slug === val.slug )
         {
           $scope.mp3 = val.mp3;
+          $scope.song = val;
         }
       })
   }]);
--- app/js/services.js
+++ app/js/services.js
@@ -9,8 +9,8 @@ angular.module('myApp.services', [])
   .value('version', '0.1')
   .factory('Songs', function () {
   	return [
+	  	{'slug':'alicecamel', 'title':'Alice the Camel', 'mp3':'http://kids.niehs.nih.gov/games/songs/childrens/alicecamel.mp3'},
 	  	{'slug':'frere', 'title':'Frere Jacques', 'mp3':'http://kids.niehs.nih.gov/games/songs/childrens/frere.mp3'},
-	  	{'slug':'oldmac', 'title':'Old Macdonald', 'mp3':'http://kids.niehs.nih.gov/games/songs/childrens/oldmac.mp3'},
-	  	{'slug':'alicecamel', 'title':'Alice the Camel', 'mp3':'http://kids.niehs.nih.gov/games/songs/childrens/alicecamel.mp3'}
+	  	{'slug':'oldmac', 'title':'Old Macdonald', 'mp3':'http://kids.niehs.nih.gov/games/songs/childrens/oldmac.mp3'}
   	];
-  })
+  });
